9df3b1113f0d0f250c6b8496eb8afe9d086c21e7
Kush Patel <kushpatel36@gmail.com>
2023-07-05 19:21:04 +0530

    sign in and sign up component is complete

---
+++ src/firebase.config.js
@@ -0,0 +1,20 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getFirestore } from 'firebase/firestore'
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyBam-xdIVZXIy3_nX4zPswOyZ77LV8d6lU",
+    authDomain: "house-marketplace-app-38fbd.firebaseapp.com",
+    projectId: "house-marketplace-app-38fbd",
+    storageBucket: "house-marketplace-app-38fbd.appspot.com",
+    messagingSenderId: "916103057941",
+    appId: "1:916103057941:web:6f52a7515490c67428b41c"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export const db = getFirestore()
+
