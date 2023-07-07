55aa154f922cbb5cc26fa9f6c982cbeb2fba4171
Kush Patel <kushpatel36@gmail.com>
2023-07-07 19:30:13 +0530

    just start the profile detailing

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,11 +1,35 @@
-import React from 'react';
+import { getAuth } from 'firebase/auth';
+import React, { useEffect, useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 
 const Profile = () => {
-    return (
-        <div>
-            <h1>Profile</h1>
-        </div>
-    );
+    const auth = getAuth()
+    const [chageDetails, setChangeDetails] = useState(false)
+    const [formData, setFormData] = useState({
+        name: auth.currentUser.displayName,
+        email: auth.currentUser.email
+    })
+
+    const { name, email } = formData
+
+    const navigate = useNavigate()
+
+    const onLogout = () => {
+        auth.signOut()
+        navigate('/')
+    }
+
+    return <div className='profile'>
+        <header className="profileHeader">
+            <p className="pageHeader">My Profile</p>
+            <button type='button' className="logOut" onClick={onLogout}>Logout</button>
+        </header>
+        <main>
+            <div className="profileDetailsHeader">
+                <p className="profileDetailsText">{ }</p>
+            </div>
+        </main>
+    </div>
 };
 
 export default Profile;
